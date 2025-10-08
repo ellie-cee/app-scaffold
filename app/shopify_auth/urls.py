e9551832da178572a0c83a5e5ca9a2ba3821cace
Ellie Cassady <cassadyeleanor@gmail.com>
2025-10-08 16:52:20 -0700

    post-merge clarity

--- app/shopify_auth/urls.py
+++ app/shopify_auth/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='shopify_auth_login'),
-    path('authenticate/', views.authenticate, name='shopify_auth_authenticate'),
-    path('finalize/', views.finalize, name='shopify_auth_login_finalize'),
-    path('logout/', views.logout, name='shopify_auth_logout'),
+    path('login', views.login, name='shopify_auth_login'),
+    path('authenticate', views.authenticate, name='shopify_auth_authenticate'),
+    path('finalize', views.finalize, name='shopify_auth_login_finalize'),
+    path('logout', views.logout, name='shopify_auth_logout'),
 ]
